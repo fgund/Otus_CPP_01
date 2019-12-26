c32db7739a445708e6ae856ae0215caa645dc2ea
Filipp Gundelakh <f.gundelakh@yandex.ru>
2019-12-26 05:22:38 +0300

    Trying gtest

---
+++ g_test_version.cpp
@@ -0,0 +1,13 @@
+#include "lib.h"
+
+#include <gtest/gtest.h>
+
+TEST(test_version, test_valid_version)
+{
+    ASSERT_GT(version(), 0);
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
